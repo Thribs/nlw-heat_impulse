16484ad346b26c9df126fef15ee44fd8ea05eb47
ThribsClube <thiago@clubedochaveiro.com.br>
2021-11-10 17:12:07 -0300

    database is messed up. Fixing soon

---
+++ heat_tags/lib/heat_tags_web/controllers/messages_controller.ex
@@ -0,0 +1,11 @@
+defmodule HeatTagsWeb.MessagesController do
+  use HeatTagsWeb, :controller
+
+  def create(conn, params) do
+    IO.inspect(params)
+
+    # conn
+    # |> text("RECEBI A REQUISIÇÃO")
+    text(conn, "RECEBI A REQUISIÇÃO")
+  end
+end
